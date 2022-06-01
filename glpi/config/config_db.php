b3d3a7a56b9cddc6d331662663393944d1698542
Rubem Silva <rubekx@gmail.com>
2022-06-01 17:43:26 -0300

    update glpi database credentials

--- glpi/config/config_db.php
+++ glpi/config/config_db.php
@@ -1,7 +1,7 @@
 <?php
 class DB extends DBmysql {
    public $dbhost     = 'mariadb';
-   public $dbuser     = 'glpi_user';
-   public $dbpassword = 'glpi';
+   public $dbuser     = 'root';
+   public $dbpassword = 'diouxx';
    public $dbdefault  = 'glpidb';
 }
